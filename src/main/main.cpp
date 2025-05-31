df7a29046b389866e946cc7eafbb92da10487994
Thibault Clodion <thibaultclodion@gmail.com>
2025-05-31 11:40:04 +0200

    First test with imgui

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <iostream> // For temporary std::cout
-
+#include <imgui-SFML.h>
+#include <imgui.h>
 #include "Param.h"
 #include "Map.h"
 
+
+void DebugTest() { std::cout << "Debug test executed!" << std::endl; }
 
 int main() 
 {
@@ -13,11 +16,20 @@ int main()
   sf::RenderWindow window{sf::VideoMode({WINDOW_HEIGHT, WINDOW_WIDTH}),"Jeu De La Nature"};
   window.setFramerateLimit(30);
 
+  // Initialize ImGui-SFML
+  if (!ImGui::SFML::Init(window)) return -1;
+
+  // Set background color
+  sf::RectangleShape background(sf::Vector2f(WINDOW_HEIGHT, WINDOW_WIDTH));
+  background.setFillColor(sf::Color::White);
+
+  sf::Clock deltaClock;
   // run the program as long as the window is open
   while (window.isOpen()) 
   {
     while (const std::optional event = window.pollEvent()) 
     {
+      ImGui::SFML::ProcessEvent(window, *event);
       // "close requested" event: we close the window
       if (event->is<sf::Event::Closed>()) window.close();
 
@@ -28,22 +40,29 @@ int main()
       }
     }
 
+    // ImGui-SFML - Test
+    ImGui::SFML::Update(window, deltaClock.restart());
+    ImGui::Begin("Hello, world!");
+    if (ImGui::Button("Debug test")) 
+      DebugTest();
+    ImGui::End();
+
     window.clear();
 
-    sf::RectangleShape background(sf::Vector2f(1600.f, 1600.f));
-    background.setFillColor(sf::Color::White);
+    // Draw the background and the map
     window.draw(background);
-
-    // Draw the map
     map.Draw(window);
+    ImGui::SFML::Render(window);
 
     window.display();
     
     // Sleep for 5s and log next generation
-    sf::sleep(sf::seconds(5));
-    std::cout << "Next generation..." << std::endl;
-    map.NextGeneration();
+    //sf::sleep(sf::seconds(5));
+    //std::cout << "Next generation..." << std::endl;
+    //map.NextGeneration();
   }
+  
+  ImGui::SFML::Shutdown();
 
   return 0;
 }
